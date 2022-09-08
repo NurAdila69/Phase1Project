20e0a21d811ba2d38a190cbb93418aa68c6a9518
nurAdila69 <Nur.Adam-daud@mindweaver.co.uk>
2022-09-08 15:03:07 +0100

    created a feature to display options to the user

--- src/Project1.java
+++ src/Project1.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Project1 {
+    public static void main(String[] args) {
+        // Writing option menu for users
+
+        System.out.println("1. List all files");
+        System.out.println("2. Detailed options");
+        System.out.println("3. Close application");
+
+        System.out.println( "Please select one of the following options:");
+        // scanner is the name I gave to Scanner, then choose new scanner
+        Scanner scanner = new Scanner(System.in);
+        // reads the user input and stores it in the variable
+        String userInput= scanner.nextLine();
+     
+    }
 }
